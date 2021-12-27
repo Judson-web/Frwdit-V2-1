62aee426528c1dec60d91c9e9119cabf893abae9
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2021-12-27 13:14:05 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,45 +2,47 @@ import os
 from config import Config
 
 class Translation(object):
-  START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot V2
-This Bot forward all files to One Public channel to Your Personal channel
-More details /help</i>"""
+  START_TXT = """<b>Hᴀɪ {}!!</b>
+<i>I'ᴍ Sɪᴍᴘʟᴇ Aᴜᴛᴏ ғɪʟᴇ Fᴏʀᴡᴀʀᴅ Bᴏᴛ V2
+Tʜɪs Bᴏᴛ ғᴏʀᴡᴀʀᴅ ᴀʟʟ ғɪʟᴇs ᴛᴏ Oɴᴇ Pᴜʙʟɪᴄ ᴄʜᴀɴɴᴇʟ ᴛᴏ Yᴏᴜʀ Pᴇʀsᴏɴᴀʟ ᴄʜᴀɴɴᴇʟ
+Mᴏʀᴇ ᴅᴇᴛᴀɪʟs /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
-  HELP_TXT = """<b>Follow These Steps!!
-• User Join From channel Must(No need Admin permission)
-• Then give admin permission in your personal telegram channel
-• Then use /run command in your bot</b>
+  HELP_TXT = """<b>Fᴏʟʟᴏᴡ Tʜᴇsᴇ Sᴛᴇᴘs!!
+• Usᴇʀ Jᴏɪɴ Fʀᴏᴍ ᴄʜᴀɴɴᴇʟ Mᴜsᴛ(Nᴏ ɴᴇᴇᴅ Aᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ)
+• Tʜᴇɴ ɢɪᴠᴇ ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ ɪɴ ʏᴏᴜʀ ᴘᴇʀsᴏɴᴀʟ ᴛᴇʟᴇɢʀᴀᴍ ᴄʜᴀɴɴᴇʟ
+• Tʜᴇɴ ᴜsᴇ /run ᴄᴏᴍᴍᴀɴᴅ ɪɴ ʏᴏᴜʀ ʙᴏᴛ</b>
 
-<b><u>Available Command</b></u>
+<b><u>Aᴠᴀɪʟᴀʙʟᴇ Cᴏᴍᴍᴀɴᴅ</b></u>
 
-* /start - <i>Bot Alive</i>
-* /help - <i>more help</i>
-* /run - <i>start forward</i>
-* /about - <i>About Me</i>
-* /restart - <i>Server Restart</i>"""
-  ABOUT_TXT = """<b><u>My Info</b></u>
+* /start - <i>Bᴏᴛ Aʟɪᴠᴇ</i>
+* /help - <i>Mᴏʀᴇ Hᴇʟᴘ</i>
+* /run - <i>Sᴛᴀʀᴛ Fᴏʀᴡᴀʀᴅ</i>
+* /about - <i>Aʙᴏᴜᴛ Mᴇ</i>
+* /restart - <i>Sᴇʀᴠᴇʀ Rᴇsᴛᴀʀᴛ</i>"""
+  ABOUT_TXT = """
+╔════❰ Aʙᴏᴜᴛ ❱═❍⊱❁۪۪
+║╭━━━━━━━━━━━━━━━➣ 
+║┣⪼ Mʏ Nᴀᴍᴇ - <a href="http://t.me/ForwardProV2Robot">Fᴏʀᴡᴀʀᴅ Pʀᴏ</a>
+║┣⪼ Cʀᴇᴀᴛᴏʀ - <a href=https://t.me/VAMPIRE_KING_NO_1>ƬЄƦƦƠƦ MƖƇƘЄƳ</a>
+║┣⪼ Lɪʙʀᴀʀʏ - <a href="https://docs.pyrogram.org/"> Pʏʀᴏɢʀᴀᴍ </a>
+║┣⪼ Lᴀɴɢᴜᴀɢᴇ - Pʏᴛʜᴏɴ 3 
+║┣⪼ ⲂⲞⲦ Sᴇʀᴠᴇʀ - Hᴇʀᴏᴋᴜ
+║╰━━━━━━━━━━━━━━━➣ ╚══════════════════❍⊱❁۪۪
+"""
+  FROM_MSG = "<b><u>Sᴇᴛ Fʀᴏᴍ Cʜᴀɴɴᴇʟ Usᴇʀɴᴀᴍᴇ</b></u>\n<b>Eɴᴛᴇʀ Fʀᴏᴍ Cʜᴀɴɴᴇʟ Usᴇʀɴᴀᴍᴇ</b>\n<code>ᴇɢ: @username</code>\n/cancel <code>- Cᴀɴᴄᴇʟ ᴛʜɪs ᴘʀᴏᴄᴇss</code>"
+  TO_MSG = "<b><u>Sᴇᴛ Tᴏ Cʜᴀɴɴᴇʟ ID</b></u>\n<b>Eɴᴛᴇʀ Tᴏ Cʜᴀɴɴᴇʟ ɪᴅ</b>\n<code>ᴇɢ: -100xxxxxxxxxx</code>\n/cancel <code>- Cᴀɴᴄᴇʟ ᴛʜɪs ᴘʀᴏᴄᴇss</code>"
+  SKIP_MSG = "<b><u>Sᴇᴛ Fɪʟᴇ Sᴋɪᴘɪɴɢ Nᴜᴍʙᴇʀ</b></u>\n<b>Sᴋɪᴘ ᴛʜᴇ ғɪʟᴇ ᴀs ᴍᴜᴄʜ ᴀs ʏᴏᴜ ᴇɴᴛᴇʀ ᴛʜᴇ ɴᴜᴍʙᴇʀ ᴀɴᴅ ᴛʜᴇ ʀᴇsᴛ ᴏғ ᴛʜᴇ ғɪʟᴇ ᴡɪʟʟ ʙᴇ ғᴏʀᴡᴀʀᴅᴇᴅ\nDᴇғᴀᴜʟᴛ Sᴋɪᴘ Nᴜᴍʙᴇʀ =</b> <code>0</code>\n<code>ᴇɢ: Yᴏᴜ ᴇɴᴛᴇʀ 0 = 0 ғɪʟᴇ sᴋɪᴘᴇᴅ\n Yᴏᴜ ᴇɴᴛᴇʀ 5 = 5 ғɪʟᴇ sᴋɪᴘᴇᴅ</code>\n/cancel <code>- Cᴀɴᴄᴇʟ ᴛʜɪs ᴘʀᴏᴄᴇss</code>"
+  LIMIT_MSG = "<b><u>Sᴇᴛ Fɪʟᴇ Fᴏʀᴡᴀʀᴅ Lɪᴍɪᴛ</u></b>\n<b>Hᴇʀᴏᴋᴜ Dᴀɪʟʏ Lɪᴍɪᴛ</b> : <code>23000</code>\n<b>Dᴇғᴀᴜʟᴛ Lɪᴍɪᴛ</b> : <code>0</code>"
+  CANCEL = "<b>Pʀᴏᴄᴇss Cᴀɴᴄᴇʟʟᴇᴅ Sᴜᴄᴄᴇғᴜʟʟʏ\nEɴᴛᴇʀ /run Aɢᴀɪɴ</b>"
+  USERNAME = "<b>Sᴇɴᴅ Usᴇʀɴᴀᴍᴇ ᴡɪᴛʜ @</b>\n<code>ᴇɢ: @Username</code>\n<b>Eɴᴛᴇʀ /run Aɢᴀɪɴ</b>"
+  INVALID_CHANNELID = "<b>Sᴇɴᴅ ᴄʜᴀɴɴᴇʟ ɪᴅ ᴡɪᴛʜ -100</b>\n<code>ᴇɢ: -100xxxxxxxxxx</code>\n<b>Eɴᴛᴇʀ /run Aɢᴀɪɴ</b>"
+  DOUBLE_CHECK = """<b><u>Dᴏᴜʙʟᴇ Cʜᴇᴄᴋɪɴɢ⚠️</b></u>
+<code>Bᴇғᴏʀᴇ ғᴏʀᴡᴀʀᴅɪɴɢ ᴛʜᴇ ғɪʟᴇ Cʟɪᴄᴋ ᴛʜᴇ Yᴇs ʙᴜᴛᴛᴏɴ ᴏɴʟʏ ᴀғᴛᴇʀ ᴄʜᴇᴄᴋɪɴɢ ᴛʜᴇ ғᴏʟʟᴏᴡɪɴɢ</code>
 
-<b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
-<b>Language :</b> <code>Python3</code>
-<b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
-<b>Server :</b> <code>Heroku</code>
-<b>Build :</b> <code>V2.0</code>"""
-  FROM_MSG = "<b><u>SET FROM CHANNEL USERNAME</b></u>\n<b>Enter From Channel Username</b>\n<code>eg: @username</code>\n/cancel <code>- Cancel this process</code>"
-  TO_MSG = "<b><u>SET TO CHANNEL ID</b></u>\n<b>Enter To Channel id</b>\n<code>eg: -100xxxxxxxxxx</code>\n/cancel <code>- Cancel this process</code>"
-  SKIP_MSG = "<b><u>SET FILE SKIPING NUMBER</b></u>\n<b>Skip the file as much as you enter the number and the rest of the file will be forwarded\nDefault Skip Number =</b> <code>0</code>\n<code>eg: You enter 0 = 0 file skiped\n You enter 5 = 5 file skiped</code>\n/cancel <code>- Cancel this process</code>"
-  LIMIT_MSG = "<b><u>SET FILE FORWARD LIMIT</u></b>\n<b>Heroku Daily Limit</b> : <code>23000</code>\n<b>Default Limit</b> : <code>0</code>"
-  CANCEL = "<b>Process Cancelled Succefully\nEnter /run Again</b>"
-  USERNAME = "<b>Send Username with @</b>\n<code>eg: @Username</code>\n<b>Enter /run Again</b>"
-  INVALID_CHANNELID = "<b>Send channel id with -100</b>\n<code>eg: -100xxxxxxxxxx</code>\n<b>Enter /run Again</b>"
-  DOUBLE_CHECK = """<b><u>DOUBLE CHECKING ⚠️</b></u>
-<code>Before forwarding the file Click the Yes button only after checking the following</code>
+<i>° Mᴜsᴛ ʙᴇ ᴀ ᴜsᴇʀ ᴊᴏɪɴ ɪɴ Fʀᴏᴍ ᴄʜᴀɴɴᴇʟ ᴄʜᴇᴄᴋ ᴛʜᴀᴛ sᴛᴀᴛᴜs</i>
+<i>Usᴇʀ ᴊᴏɪɴ ᴛʜɪs ᴄʜᴀɴɴᴇʟ : <b>{}</b></i>
+<b><u>Nᴏᴛᴇ</u></b> : <i>Aᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ ɪs ɴᴏᴛ ᴍᴀɴᴅᴀᴛᴏʀʏ</i>
+<i>° Aᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴ ɪs ᴍᴀɴᴅᴀᴛᴏʀʏ ғᴏʀ ᴛʜᴇ ʙᴏᴛ ᴏɴ ᴛʜᴇ Tᴏ ᴄʜᴀɴɴᴇʟ ᴄʜᴇᴄᴋ ᴛʜᴀᴛ sᴛᴀᴛᴜs</i>
+<b><u>Nᴏᴛᴇ</u></b> : <i>Tʜᴇʀᴇ ɪs ɴᴏ ʀᴇᴏ̨ᴜɪʀᴇᴍᴇɴᴛ ғᴏʀ ᴀ ᴜsᴇʀ ᴛᴏ ᴊᴏɪɴ ᴛʜᴇ Tᴏ ᴄʜᴀɴɴᴇʟ</i>
 
-<i>° Must be a user join in From channel check that status</i>
-<i>User join this channel : <b>{}</b></i>
-<b><u>Note</u></b> : <i>Admin permission is not mandatory</i>
-<i>° Admin permission is mandatory for the bot on the To channel check that status</i>
-<b><u>Note</u></b> : <i>There is no requirement for a user to join the To channel</i>
-
-<b>If the above is checked then the yes button can be clicked</b>"""
+<b>Iғ ᴛʜᴇ ᴀʙᴏᴠᴇ ɪs ᴄʜᴇᴄᴋᴇᴅ ᴛʜᴇɴ ᴛʜᴇ ʏᴇs ʙᴜᴛᴛᴏɴ ᴄᴀɴ ʙᴇ ᴄʟɪᴄᴋᴇᴅ</b>"""
